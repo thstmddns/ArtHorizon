2d7284e43c6c3c58790b62396bd7f6d4b2969304
박자연 <hosan130@yu.ac.kr>
2022-09-15 14:05:26 +0900

    back test

--- back/src/main/java/com/ssafy/arthorizon/BackApplication.java
+++ back/src/main/java/com/ssafy/arthorizon/BackApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackApplication {
 
+	// 여기서 올리면 진짜최종제대로가야함
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
 	}

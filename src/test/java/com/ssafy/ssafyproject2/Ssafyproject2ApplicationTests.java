f691c80b3ff3087b9b9b1233ac79d59f60b04f4e
박자연 <hosan130@yu.ac.kr>
2022-09-15 10:00:49 +0900

    init commit2

---
+++ src/test/java/com/ssafy/ssafyproject2/Ssafyproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafyproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssafyproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

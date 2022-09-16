200dfb0015eafa56f72e54c014f94a010f0553bf
JaeHyuk Han <jhhan2206@naver.com>
2022-09-16 15:14:33 +0900

    [S07P22D201-29] feat-fe: login

--- frontend/src/Router/Login.jsx
+++ frontend/src/Router/Login.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const LoginDiv = styled.div`
+  background-image: 'url(../../public/images/backgroundImage.jpg)';
+  `
 
 const Login = () => {
     return (
-        <div>
-            Here is login page.
-        </div>
+      <div>
+        <LoginDiv>
+          Here is Login, Right?
+        </LoginDiv>
+      </div>
     )
 }
 

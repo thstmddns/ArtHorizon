039158787324f56a7c6a06a0ae7fc549329a856e
hhejo <gkagmlwn94@naver.com>
2022-09-20 22:04:21 +0900

    [S07P22D201-28] feat-fe/signup: SignUp page design

---
+++ frontend/src/components/form/FormErrorMessage.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const FormErrorMessage = styled.span`
+  font-size: 0.8rem;
+  height: 20px;
+  display: inline-flex;
+  align-items: center;
+  margin-left: 12px;
+  color: #db5858;
+`;
+
+export default FormErrorMessage;

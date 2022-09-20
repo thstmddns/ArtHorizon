a76598f2bac72f498ebd958653441b736c625848
hhejo <gkagmlwn94@naver.com>
2022-09-20 11:28:48 +0900

    [S07P22D201-35] feat-fe/login: 로그인 화면 구현

--- frontend/src/Router/Login.jsx
+++ frontend/src/Router/Login.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 
-const LoginDiv = styled.div`
-  background-image: "url(../../public/images/backgroundImage.jpg)";
-`;
+import LoginForm from "../login/LoginForm";
+
+import loginBackgroundImage from "../images/loginBackgroundImage.jpg";
 
 const Login = () => {
   return (
-    <div>
-      <LoginDiv>Here is Login, Right?</LoginDiv>
-    </div>
+    <Container>
+      <LeftDiv />
+      <RightDiv>
+        <LoginForm />
+      </RightDiv>
+    </Container>
   );
 };
 
 export default Login;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  background: url(${loginBackgroundImage}) no-repeat center;
+  background-size: cover;
+`;
+
+const LeftDiv = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const RightDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;

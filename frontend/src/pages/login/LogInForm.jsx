aa0c9243a2c1a495afc8958bccec832a7cbf5ddd
JaeHyuk Han <jhhan2206@naver.com>
2022-09-22 00:19:13 +0900

    [S07P22D201-34] Feat-fe: login api 연결

--- frontend/src/pages/login/LogInForm.jsx
+++ frontend/src/pages/login/LogInForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { useState } from "react";
+
 import FormWrapper from "../../components/form/FormWrapper";
 import FormTitle from "../../components/form/FormTitle";
 import FormItem from "../../components/form/FormItem";
@@ -9,23 +11,48 @@ import FormLinksWrapper from "../../components/form/FormLinksWrapper";
 import Input from "../../components/input/Input";
 import Label from "../../components/input/Label";
 
+import Login from "../../api/Login";
+
 const LogInForm = () => {
+  const { id, setId } = useState("");
+  const { password, setPassword } = useState("");
+
+  const changeId = (getId) => {
+    setId(getId);
+  };
+
+  const changePassword = (getPassword) => {
+    setPassword(getPassword);
+  };
+
+  const tryLogin = () => {
+    Login(id, password);
+  };
+
   return (
     <FormWrapper>
       <FormTitle>로그인</FormTitle>
       <FormItem>
         <Label htmlFor="email">이메일</Label>
-        <Input type="email" id="email" placeholder="이메일을 입력하세요" />
+        <Input
+          type="email"
+          id="email"
+          name="id"
+          onChange={(e) => changeId(e.target.value)}
+          placeholder="이메일을 입력하세요"
+        />
       </FormItem>
       <FormItem>
         <Label htmlFor="password">비밀번호</Label>
         <Input
           type="password"
           id="password"
+          name="password"
+          onChange={(e) => changePassword(e.target.value)}
           placeholder="비밀번호를 입력하세요"
         />
       </FormItem>
-      <LogInButton>로그인하기</LogInButton>
+      <LogInButton onClick={tryLogin}>로그인하기</LogInButton>
       <FormItem>
         <FormLinksWrapper>
           <MyLink>비밀번호 찾기</MyLink>

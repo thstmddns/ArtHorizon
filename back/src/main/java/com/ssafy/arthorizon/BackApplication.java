f6e8074452f5978312b3b575220fab69002edd54
이주영 <ph2978@naver.com>
2022-09-22 14:29:56 +0900

    fix : CORS

--- back/src/main/java/com/ssafy/arthorizon/BackApplication.java
+++ back/src/main/java/com/ssafy/arthorizon/BackApplication.java
@@ -2,6 +2,9 @@ package com.ssafy.arthorizon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackApplication {
@@ -11,4 +14,13 @@ public class BackApplication {
 		SpringApplication.run(BackApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://j7d201.p.ssafy.io:8081");
+			}
+		};
+	}
 }

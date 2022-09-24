a1e2907486d5f36c64d9b76d1f14afc82de76e8d
hhejo <gkagmlwn94@naver.com>
2022-09-24 09:14:39 +0900

    feat-fe/mypage: 마이페이지 초안

--- frontend/src/pages/mypage/MyPage.jsx
+++ frontend/src/pages/mypage/MyPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+
+import Info from "./components/Info";
+import Arts from "./components/Arts";
 
 const Mypage = () => {
-  return <div>Here is my page.</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Info />
+        <Arts />
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Mypage;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  // background-color: tomato;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 5%;
+  margin-left: 10%;
+  margin-right: 10%;
+  // background-color: wheat;
+`;

f311527401580fa28aa3686c3dbb0cba6d9e8849
함희주 <gkagmlwn94@naver.com>
2022-09-24 14:54:41 +0900

    Feat fe/login

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,36 +1,24 @@
 import axios from "axios";
-import store from "../redux/store";
+// import store from "../redux/store";
 
-const baseURL = "http://j7d201.p.ssafy.io:8081";
+// const baseURL = "http://j7d201.p.ssafy.io:8081";
+const baseURL = "";
 
-// axios.defaults.withCredentials = true;
+// "proxy": "http://j7d201.p.ssafy.io:8081"
+
+axios.defaults.withCredentials = true;
 
 const api = axios.create({
   baseURL,
   // timeout: 1000,
   headers: {
     withCredentials: true,
-    // accept: "application/json,",
-    // "Content-Type": "application/json;charset=UTF-8",
+    accept: "application/json,",
+    "Content-Type": "application/json;charset=UTF-8",
   },
 });
 
 // axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-
-api.interceptors.request.use(
-  (config) => {
-    const token = store.getState().Auth.token;
-    try {
-      if (token && jwtUtils.isAuth(token)) {
-        config.headers.Authorization = `jwt ${token}`;
-      }
-      return config;
-    } catch (error) {
-      console.error("[_axios.interceptors.request] config: " + error);
-    }
-  },
-  (error) => Promise.reject(error)
-);
 
 // api.interceptors.request.use((config) => {
 //   if (!config.headers.authorization) {
@@ -43,8 +31,8 @@ api.interceptors.request.use(
 //   return config;
 // });
 
-const token = store.getState().Auth.token;
+// const token = store.getState().Auth.token;
 
 export const authApi = {
-  login: (loginData) => api.post("/login", loginData),
+  login: (loginData) => api.post("/users/login", loginData),
 };

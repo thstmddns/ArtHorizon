356cf428eace35e3f76d658adf2de60c6eba07c1
Han JaeHyuk <jhhan2206@naver.com>
2022-09-26 14:24:25 +0900

    [S07P22D201-149] Feat-fe: 메인화면 정돈

--- frontend/src/components/piece/Card.jsx
+++ frontend/src/components/piece/Card.jsx
@@ -4,7 +4,8 @@ import styled from "styled-components";
 const Card = (props) => {
   return (
     <CardWrapper>
-      <RadiusImg src={props.image} />
+      {/* <RadiusImg src={props.image} alt="pic" /> */}
+      <div>Card will here</div>
     </CardWrapper>
   );
 };
@@ -15,12 +16,10 @@ const CardWrapper = styled.div`
   background: #f5f5f5;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
-  width: 350px;
-  hight: 350px;
 `;
 
 const RadiusImg = styled.img`
   border-radius: 10px;
-  max-height: 350px;
-  max-width: 350px;
+  max-height: 280px;
+  max-width: 280px;
 `;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 
 import NavigationBar from "../../components/NavigationBar";
 import SearchBar from "../../components/serach/SerachBar";
+import { Container, Row, Col } from "react-grid-system";
 import Card from "../../components/piece/Card";
 import baseurl from "../../api/BaseUrl";
 // import Newart from "../../api/Newart";
@@ -30,15 +31,28 @@ const Home = () => {
       });
   };
 
-  useEffect(() => {
-    const content = Newart(1);
-    console.log(content);
-    // setPictures(content.pieceList);
-  }, []);
+  // useEffect(() => {
+  //   const content = Newart(1);
+  //   console.log(content);
+  //   // setPictures(content.pieceList);
+  // }, []);
+
+  const testrender = () => {
+    const result = [];
+    for (let i = 1; i < 9; i++) {
+      result.push(
+        <CardCol sm={3}>
+          <Card />
+        </CardCol>
+      );
+    }
+    return result;
+  };
+
   return (
     <Main>
       <NavigationBar />
-      <Container>
+      <MainContainer>
         <SearchBar />
         <div>
           <hr />
@@ -47,13 +61,15 @@ const Home = () => {
               <Title>태그 추천</Title>
             </TopText>
           </div>
+          <hr />
           <div className="NewArt">
             <TopText>
               <Title>최근 등록된 작품</Title>
               <Link to="pieces">더 보기</Link>
             </TopText>
-            <div>
-              {pictures.map((picture) => {
+            <PieceWrapper>
+              <Row justify="center">{testrender()}</Row>
+              {/* {pictures.map((picture) => {
                 return (
                   <Card
                     key={picture.pieceSeq}
@@ -62,11 +78,11 @@ const Home = () => {
                     image={picture.pieceImg}
                   />
                 );
-              })}
-            </div>
+              })} */}
+            </PieceWrapper>
           </div>
         </div>
-      </Container>
+      </MainContainer>
     </Main>
   );
 };
@@ -78,7 +94,7 @@ const Main = styled.div`
   height: 100vh;
 `;
 
-const Container = styled.div`
+const MainContainer = styled.div`
   padding: 0 5vw;
 `;
 
@@ -90,4 +106,14 @@ const TopText = styled.div`
 const Title = styled.div`
   font-size: 20px;
   font-weight: bold;
+`;
+
+const PieceWrapper = styled(Container)`
+  margin: 3vw 1vw;
+`;
+
+const CardCol = styled(Col)`
+  width: 280px;
+  height: 280px;
+  padding: auto 1vw;
 `;

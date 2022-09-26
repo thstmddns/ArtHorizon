ede86f41bdad93ad6b534e984b132bf092fe38a0
이주영 <ph2978@naver.com>
2022-09-26 14:01:20 +0900

    CORS 해결하기

--- back/src/main/java/com/ssafy/arthorizon/user/UserController.java
+++ back/src/main/java/com/ssafy/arthorizon/user/UserController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {

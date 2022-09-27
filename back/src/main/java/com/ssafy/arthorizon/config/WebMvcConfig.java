dd40af28a937923c96cacf3588ecd728161bb7a1
이주영 <ph2978@naver.com>
2022-09-27 14:57:56 +0900

    refactor : allowedOrigins 수정

--- back/src/main/java/com/ssafy/arthorizon/config/WebMvcConfig.java
+++ back/src/main/java/com/ssafy/arthorizon/config/WebMvcConfig.java
@@ -34,7 +34,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
                 .allowCredentials(true)

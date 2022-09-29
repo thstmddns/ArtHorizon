ed029530331f60e7e97fd0af68e5fc47aa4d824c
hhejo <gkagmlwn94@naver.com>
2022-09-29 17:10:44 +0900

    feat-fe/pieces: 작품목록 페이지 구성

--- frontend/src/components/TailwindNavBar.jsx
+++ frontend/src/components/TailwindNavBar.jsx
@@ -20,32 +20,34 @@ const TailwindNavBar = () => {
   }, [dispatch, isLoggedIn]);
 
   return (
-    <header class="body-font shadow-md">
-      <div class="mx-5 flex flex-wrap py-3 flex-col md:flex-row items-center">
+    <nav className="body-font shadow-md">
+      <div className="mx-5 flex flex-wrap py-3 flex-col md:flex-row items-center">
         <Link
           to="/"
-          class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
         >
-          <img src={logoImage} class="w-30 h-10" alt="logoImage" />
+          <img src={logoImage} className="w-30 h-10" alt="logoImage" />
         </Link>
-        <nav class="text-gray-600 md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-          <Link class="mr-5 hover:text-gray-900">작품 목록</Link>
-          <Link class="mr-5 hover:text-gray-900">스타일 트랜스퍼</Link>
-          <Link class="mr-5 hover:text-gray-900">그림의 향</Link>
-          <Link class="mr-5 hover:text-gray-900">고객센터</Link>
+        <nav className="text-gray-600 md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+          <Link to={"/pieces"} className="mr-5 hover:text-gray-900">
+            작품 목록
+          </Link>
+          <Link className="mr-5 hover:text-gray-900">스타일 트랜스퍼</Link>
+          <Link className="mr-5 hover:text-gray-900">그림의 향</Link>
+          <Link className="mr-5 hover:text-gray-900">고객센터</Link>
         </nav>
 
         {!isLoggedIn && (
           <div>
             <button
               onClick={() => navigate("/login")}
-              class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 mr-1"
+              className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 mr-1"
             >
               로그인
             </button>
             <button
               onClick={() => navigate("/signup")}
-              class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+              className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
             >
               회원가입
             </button>
@@ -56,20 +58,20 @@ const TailwindNavBar = () => {
           <div>
             <button
               onClick={() => navigate(`/mypage/${userSeq}`)}
-              class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 mr-1"
+              className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 mr-1"
             >
               {myNickname}'s 페이지
             </button>
             <button
               onClick={() => dispatch(logout())}
-              class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+              className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
             >
               로그아웃
             </button>
           </div>
         )}
       </div>
-    </header>
+    </nav>
   );
 };
 

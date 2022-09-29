74e059786c518d58250cb11031c1ecd7c60b1e26
박자연 <hosan130@yu.ac.kr>
2022-09-29 14:35:44 +0900

    [S07P22D201-147] Feat-be : 이미지 파일 입출력 (/api/my-file)

--- back/src/main/java/com/ssafy/arthorizon/file/FileController.java
+++ back/src/main/java/com/ssafy/arthorizon/file/FileController.java
@@ -15,7 +15,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
-@RequestMapping("/api/file")
+@RequestMapping("/api/my-file")
 public class FileController {
 
     private final String SUCCESS = "SUCCESS";

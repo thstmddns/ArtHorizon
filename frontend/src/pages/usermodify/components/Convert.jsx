05c70e0cfd293dc5ee38d2d0d3d51f26cbddabe8
hhejo <gkagmlwn94@naver.com>
2022-10-04 12:57:54 +0900

    feat-fe/mypage: 마이페이지, 회원정보수정, 로그인, 회원가입 등

--- frontend/src/pages/usermodify/components/Convert.jsx
+++ frontend/src/pages/usermodify/components/Convert.jsx
@@ -14,9 +14,10 @@ const Convert = (props) => {
   };
 
   return (
-    <form method="post" onSubmit={submitHandler}>
-      <TabTitle>화가로 전환</TabTitle>
-      {/* <FormItem>
+    <React.Fragment>
+      <form method="post" onSubmit={submitHandler}>
+        <TabTitle>화가로 전환</TabTitle>
+        {/* <FormItem>
         <Label htmlFor="toArtist">화가로..</Label>
         <Input
           type="text"
@@ -25,8 +26,52 @@ const Convert = (props) => {
           placeholder="변경할 닉네임을 입력하세요"
         />
       </FormItem> */}
-      <button>테스트버튼</button>
-    </form>
+        <button>테스트버튼</button>
+      </form>
+
+      <div
+        className="text-2xl text-gray-700 font-bold border-solid border-b border-gray-300 mb-8"
+        data-aos="fade-left"
+      >
+        화가로 전환
+      </div>
+      <form method="post" onSubmit={submitHandler} data-aos="fade-up">
+        <div className="relative mb-4">
+          <label htmlFor="password1" className="leading-7 text-gray-900">
+            기존 비밀번호
+          </label>
+          <input
+            type="password"
+            id="password1"
+            name="password1"
+            className="w-full bg-gray-50 rounded-lg border border-gray-300 focus:border-amber-500 focus:bg-white focus:ring-2 focus:ring-amber-200 outline-none text-gray-700 py-1 px-3 leading-8 transition"
+            placeholder="기존 비밀번호를 입력하세요"
+          />
+        </div>
+
+        <label
+          for="Toggle1"
+          className="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-100"
+        >
+          <span>Left</span>
+          <span className="relative">
+            <input id="Toggle1" type="checkbox" className="hidden peer" />
+            <div className="w-10 h-6 rounded-full shadow-inner dark:bg-gray-400 peer-checked:dark:bg-violet-400"></div>
+            <div className="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto dark:bg-gray-800"></div>
+          </span>
+          <span>Right</span>
+        </label>
+
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="flex text-white bg-amber-400 border-0 py-2 px-8 focus:outline-none hover:bg-amber-500 active:bg-amber-600 focus:ring focus:ring-amber-300 rounded-lg text-lg transition"
+          >
+            변경하기
+          </button>
+        </div>
+      </form>
+    </React.Fragment>
   );
 };
 
--- frontend/src/pages/usermodify/components/Password.jsx
+++ frontend/src/pages/usermodify/components/Password.jsx
@@ -1,47 +1,35 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { useDispatch } from "react-redux";
-// import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { changePassword } from "../../../redux/authSlice";
 
-// import FormTitle from "../../../components/form/FormTitle";
-// import FormItem from "../../../components/form/FormItem";
-// import Label from "../../../components/input/Label";
-// import Input from "../../../components/input/Input";
-// import FormButton from "../../../components/form/FormButton";
-
-const Password = (props) => {
+const Password = () => {
   const dispatch = useDispatch();
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
   const [password3, setPassword3] = useState("");
-  const [errorPassword1Message, setErrorPassword1Message] = useState("");
-  const [errorPassword2Message, setErrorPassword2Message] = useState("");
-  const [errorPassword3Message, setErrorPassword3Message] = useState("");
 
   const submitHandler = (e) => {
     e.preventDefault();
-    setErrorPassword1Message("");
-    setErrorPassword2Message("");
-    setErrorPassword3Message("");
+
     let hasError = false;
 
     if (!password1.trim()) {
-      setErrorPassword1Message("기존 비밀번호를 입력하세요");
+      toast.warn("기존 비밀번호를 입력하세요");
       hasError = true;
     }
     if (!password2.trim()) {
-      setErrorPassword2Message("변경할 비밀번호를 입력하세요");
+      toast.warn("변경할 비밀번호를 입력하세요");
       hasError = true;
     }
     if (!password3.trim()) {
-      setErrorPassword3Message("변경할 비밀번호를 한 번 더 입력하세요");
+      toast.warn("변경할 비밀번호를 한 번 더 입력하세요");
       hasError = true;
     }
     if (password2 !== password3) {
-      setErrorPassword2Message("비밀번호가 일치하지 않습니다");
-      setErrorPassword3Message("비밀번호가 일치하지 않습니다");
+      toast.warn("비밀번호가 일치하지 않습니다");
       hasError = true;
     }
 
@@ -61,107 +49,69 @@ const Password = (props) => {
   };
 
   return (
-    <form method="post" onSubmit={submitHandler}>
-      <TabTitle>비밀번호 변경</TabTitle>
-      <FormControl>
-        <Label htmlFor="password1">기존 비밀번호</Label>
-        <Input
-          type="password"
-          id="password1"
-          name="password1"
-          placeholder="기존 비밀번호를 입력하세요"
-          value={password1}
-          onChange={(e) => setPassword1(e.target.value)}
-        />
-        <ErrorMessage>{errorPassword1Message}</ErrorMessage>
-      </FormControl>
-      <FormControl>
-        <Label htmlFor="password2">변경할 비밀번호</Label>
-        <Input
-          type="password"
-          id="password2"
-          name="password2"
-          placeholder="변경할 비밀번호를 입력하세요"
-          value={password2}
-          onChange={(e) => setPassword2(e.target.value)}
-        />
-        <ErrorMessage>{errorPassword2Message}</ErrorMessage>
-      </FormControl>
-      <FormControl>
-        <Label htmlFor="password3">변경할 비밀번호 확인</Label>
-        <Input
-          type="password"
-          id="password3"
-          name="password3"
-          placeholder="변경할 비밀번호를 한 번 더 입력하세요"
-          value={password3}
-          onChange={(e) => setPassword3(e.target.value)}
-        />
-        <ErrorMessage>{errorPassword3Message}</ErrorMessage>
-      </FormControl>
-      <Button type="submit">변경하기</Button>
-    </form>
+    <React.Fragment>
+      <div
+        className="text-2xl text-gray-700 font-bold border-solid border-b border-gray-300 mb-8"
+        data-aos="fade-left"
+      >
+        비밀번호 변경
+      </div>
+      <form method="post" onSubmit={submitHandler} data-aos="fade-up">
+        <div className="relative mb-4">
+          <label htmlFor="password1" className="leading-7 text-gray-900">
+            기존 비밀번호
+          </label>
+          <input
+            type="password"
+            id="password1"
+            name="password1"
+            className="w-full bg-gray-50 rounded-lg border border-gray-300 focus:border-amber-500 focus:bg-white focus:ring-2 focus:ring-amber-200 outline-none text-gray-700 py-1 px-3 leading-8 transition"
+            placeholder="기존 비밀번호를 입력하세요"
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
+          />
+        </div>
+
+        <div className="relative mb-4">
+          <label htmlFor="password2" className="leading-7 text-gray-900">
+            변경할 비밀번호
+          </label>
+          <input
+            type="password"
+            id="password2"
+            name="password2"
+            className="w-full bg-gray-50 rounded-lg border border-gray-300 focus:border-amber-500 focus:bg-white focus:ring-2 focus:ring-amber-200 outline-none text-gray-700 py-1 px-3 leading-8 transition"
+            placeholder="변경할 비밀번호를 입력하세요"
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        </div>
+
+        <div className="relative mb-4">
+          <label htmlFor="password3" className="leading-7 text-gray-900">
+            변경할 비밀번호 확인
+          </label>
+          <input
+            type="password"
+            id="password3"
+            name="password3"
+            className="w-full bg-gray-50 rounded-lg border border-gray-300 focus:border-amber-500 focus:bg-white focus:ring-2 focus:ring-amber-200 outline-none text-gray-700 py-1 px-3 leading-8 transition"
+            placeholder="변경할 비밀번호를 입력하세요"
+            value={password3}
+            onChange={(e) => setPassword3(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="flex text-white bg-amber-400 border-0 py-2 px-8 focus:outline-none hover:bg-amber-500 active:bg-amber-600 focus:ring focus:ring-amber-300 rounded-lg text-lg transition"
+          >
+            변경하기
+          </button>
+        </div>
+      </form>
+    </React.Fragment>
   );
 };
 
 export default Password;
-
-const FormControl = styled.div`
-  width: 100%;
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 15px;
-`;
-
-const Label = styled.label`
-  font-size: 1.2rem;
-  font-weight: bold;
-  margin-bottom: 15px;
-`;
-
-const Input = styled.input`
-  height: 2rem;
-  border-radius: 10px;
-  border: 1px solid #d1d7de;
-  padding: 3px 12px 3px 12px;
-  background-color: #ffffff;
-  &::placeholder {
-    color: #d9d9d9;
-  }
-`;
-
-const ErrorMessage = styled.span`
-  font-size: 0.8rem;
-  height: 20px;
-  display: inline-flex;
-  align-items: center;
-  margin-left: 12px;
-  color: #db5858;
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  background-color: #88c4e6;
-  border: 1px solid #6cb6e1;
-  border-radius: 10px;
-  color: #ffffff;
-  width: 15rem;
-  height: 45px;
-  font-size: 1.2rem;
-  font-weight: bold;
-  margin-top: 10px;
-  margin-bottom: 30px;
-  &:hover {
-    background-color: #6cb6e1;
-    border: 1px solid #88c4e6;
-  }
-`;
-
-const TabTitle = styled.h1`
-  font-size: 1.6rem;
-  font-weight: bold;
-  margin-bottom: 60px;
-  padding-bottom: 5px;
-  border-bottom: 1px solid #222529;
-`;

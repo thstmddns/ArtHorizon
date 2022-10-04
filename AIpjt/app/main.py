9d755c9bec4bf18f70db6ddf2dc8f659013bcb02
TonyWoo <do0134@naver.com>
2022-10-04 11:16:21 +0900

    fix-ai/루트 변경

--- AIpjt/app/main.py
+++ AIpjt/app/main.py
@@ -1,13 +1,10 @@
-
-
 from fastapi import FastAPI, File,UploadFile, Form
 import sys
-sys.path.append('/ST/styletransfer')
-sys.path.append('/ST/tag_recommend')
-sys.path.append('/ST/objectdetection')
-from styletransfer import style_transfer
-from tag_recommend import get_tag
-from objectdetection import detection
+sys.path.append('/S07P22D201/AIPJT/app')
+
+from ST.styletransfer import style_transfer
+from ST.tag_recommend import get_tag
+from ST.objectdetection import detection
 from pydantic import BaseModel
 import os
 from fastapi.middleware.cors import CORSMiddleware
